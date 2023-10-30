8489217a936b512e1a92de658cfdc33f95ebe867
Filip Drzewiecki <drzewieckifilip@gmail.com>
2023-10-30 22:42:53 +0100

    initial commit

---
+++ src/main/java/com/portal/controller/ItemController.java
@@ -0,0 +1,15 @@
+package com.portal.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/items")
+public class ItemController {
+
+    @GetMapping
+    public String getItems() {
+        return "TEST";
+    }
+}
